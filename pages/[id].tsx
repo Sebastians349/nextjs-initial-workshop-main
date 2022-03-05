831738d31580d082460f2a83ea9ef5f7439369d1
Sebastian Simarro <ssebastian210@gmail.com>
2022-03-05 11:27:51 -0300

    <update>datos stores MOCK

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,7 +1,22 @@
 import type {NextPage} from "next";
 
+import {useRouter} from "next/router";
+import Link from "next/link";
+
 const Store: NextPage = () => {
-  return <div>Hello Por [id]</div>;
+  const router = useRouter();
+
+  return (
+    <div>
+      Hello Store
+      <br />
+      <a href="/">Volver al inicio con anchor</a>
+      <br />
+      <Link href="/">Volver al inicio con Link</Link>
+      <br />
+      <button onClick={() => router.push("/")}>Volver al inicio con button</button>
+    </div>
+  );
 };
 
 export default Store;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type {NextPage} from "next";
 
+import {useState, useEffect} from "react";
+
+import {Store} from "../types";
 
 const Home: NextPage = () => {
-  return <div>Hello Por Index</div>;
+  const [stores, setStores] = useState<Store[]>([]);
+
+  useEffect(() => {
+    fetch("/api/stores")
+      .then((res) => res.json())
+      .then((stores: Store[]) => setStores(stores));
+  }, []);
+
+  if (!stores.length) {
+    return <span>cargando...</span>;
+  }
+
+  return <div>Hello Stores</div>;
 };
 
 export default Home;

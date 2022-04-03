76278b77a2f583d9ea9f0b243f1370d69ed6932d
Sebastian Simarro <ssebastian210@gmail.com>
2022-04-03 09:48:11 -0300

    <update>next link ok Stores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type {NextPage} from "next";
 
+import Link from "next/link";
 import {useState, useEffect} from "react";
 
+import StoreCard from "../components/StoreCard";
 import {Store} from "../types";
 
 const Home: NextPage = () => {
@@ -17,7 +19,18 @@ const Home: NextPage = () => {
     return <span>cargando...</span>;
   }
 
-  return <div>Hello Stores</div>;
+  return (
+    <main style={{display: "flex", flexDirection: "column", gap: 12}}>
+      {stores.map((store) => (
+        // eslint-disable-next-line @next/next/link-passhref
+        <Link key={store.id} href={`/${store.id}`}>
+          <a>
+            <StoreCard key={store.id} store={store} />
+          </a>
+        </Link>
+      ))}
+    </main>
+  );
 };
 
 export default Home;
